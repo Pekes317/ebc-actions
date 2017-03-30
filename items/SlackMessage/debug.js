1e17df3af1d81c2bab6b93958efbee91d1867b92
Pekes317 <El_Dante565@yahoo.com>
2017-03-29 20:53:10 -0500

    formated code

--- items/SlackMessage/debug.js
+++ items/SlackMessage/debug.js
@@ -1,20 +1,22 @@
 var handler = require('./handler.js').handler;
 var parameters = {
-    "userInput":{},
-    "dbRow" :{
-        id: 102
+    "userInput": {},
+    "dbRow": {},
+    "parameters": {
+        simulateError: true
     },
-    "parameters": {simulateError:true},
-    "userProfile":{
+    "userProfile": {
         username: "El_dante565@yahoo.com"
     }
 };
 
-handler(parameters, {succeed:function(result){
-    console.log("success: ", JSON.stringify(result, null, 2));
-    process.exit(0);
-}, fail:function(error){
-    console.error("error: ", error);
-    process.exit(1);
-}});
-
+handler(parameters, {
+    succeed: function (result) {
+        console.log("success: ", JSON.stringify(result, null, 2));
+        process.exit(0);
+    },
+    fail: function (error) {
+        console.error("error: ", error);
+        process.exit(1);
+    }
+});
